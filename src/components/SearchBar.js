248f390d1a3a0e1f433fe4d7e878d99957785589
Danijel Maksimovic <da.maksimovic@levi9.com>
2017-12-26 08:59:38 +0100

    Create SearchBar component, add editor config and eslint config files

---
+++ src/components/SearchBar.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = this.getDefaultState();
+  }
+
+  getDefaultState() {
+    return {
+      searchValue: '',
+      canSubmit: false
+    }
+  }
+
+  handleSearchSubmit = (e) => {
+    e.preventDefault();
+
+    alert('Searching');
+
+    this.setState(this.getDefaultState());
+  }
+
+  onChange = (e) => {
+    let searchValue = e.target.value;
+    let canSubmit = searchValue !== '';
+
+    this.setState({
+      searchValue,
+      canSubmit
+    });
+  } 
+
+  render() {
+    return (
+      <div className="search-bar">
+        <form onSubmit={this.handleSearchSubmit}>
+          <div className="input-group">
+            <input onChange={this.onChange} value={this.state.searchValue} type="text" className="form-control search-bar__input" placeholder="Search video..." />
+            <span className="input-group-btn">
+              <button disabled={!this.state.canSubmit} className="btn btn-primary">Search</button>
+            </span>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
